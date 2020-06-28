37b5aef79d274e4431b391e7b06279d7efc43e58
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-06-28 23:36:40 +0400

    create homepage

---
+++ testFinal/homePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const homePage =()=>{
+    return(
+        <View>
+            <Text>Hello you are in homepage))</Text>
+        </View>
+    )
+}
